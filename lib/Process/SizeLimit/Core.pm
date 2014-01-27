8a54cc15102d98e12c0676e5bbf400736fe70166
ziguzagu <ziguzagu@gmail.com>
2014-01-27 19:50:55 +0900

    Fix Mac OS X version detection on 10.9.1.

--- lib/Process/SizeLimit/Core.pm
+++ lib/Process/SizeLimit/Core.pm
@@ -161,7 +161,10 @@ BEGIN {
     }
     elsif ($Config{'osname'} =~ /darwin/i) {
         _load('BSD::Resource');
-        if (qx[sw_vers -productVersion] - 10 >= 0.9) {
+        my $ver = qx[sw_vers -productVersion] || 0;
+        chomp $ver;
+        $ver =~ s/^(\d+\.\d+)\.\d+$/$1/;
+        if ($ver - 10 >= 0.9) {
             # OSX 10.9+ has no concept of rshrd in top
             *_platform_check_size   = \&_bsd_size_check;
         }
